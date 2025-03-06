230bec88ba7f832d90b81722e6c41549c0b6feaa
meg-james <meg.james.design@gmail.com>
2025-03-05 16:59:38 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const video = document.querySelector("video");
 const filter =
-    Math.random() < 0.5 ? "rgba(255, 0, 0, 0.5)" : "rgba(0, 0, 255, 0.5)";
+    Math.random() < 0.5 ? "rgba(255, 0, 0, 0.5)" : "rgba(0, 255, 255, 0.5)";
 
 navigator.mediaDevices
     .getUserMedia({ video: { facingMode: "environment" } })
